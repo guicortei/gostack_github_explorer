a1bad01a47b11b9565b6514620486f929d60fa48
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-10-24 12:41:33 -0300

    Repository page done

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,128 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { Header, Issues, RepositoryInfo, Error } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+  const [apiError, setApiError] = useState<string>('');
+
+  useEffect(() => {
+    // ### DISPARA TUDO JUNTO E CADA UM EXECUTA SUA RESPOSTA SEPARADAMENTE
+    api
+      .get(`repos/${params.repository}`)
+      .then(response => setRepository(response.data as Repository))
+      .catch(err => setApiError(err.message));
+
+    api
+      .get(`repos/${params.repository}/issues`)
+      .then(response => setIssues(response.data as Issue[]))
+      .catch(err => setApiError(err.message));
+
+    // ### DISPARA TUDO JUNTO E CONTINUA QUANDO TUDO CHEGAR ######
+    // async function loadData(): Promise<void> {
+    //   const [repository, issues] = await Promise.all([
+    //     api.get(`repos/${params.repository}`),
+    //     api.get(`repos/${params.repository}/issues`),
+    //   ]);
+    //   console.log(repository);
+    //   console.log(issues);
+    // }
+    // loadData();
+  }, [params.repository]);
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              // src={repository ? repository.owner.avatar_url : null}
+              src={repository?.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
+
+      <Issues>
+        {issues.map(issue => (
+          <a
+            key={issue.id}
+            href={issue.html_url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Issues>
+
+      {apiError ? <Error>{apiError}</Error> : <p>Carregando</p>}
+    </>
+  );
 };
 
 export default Repository;
