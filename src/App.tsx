5751270e6052cf3c61b75c8d457331d6f8391569
Guilherme Corrêa Teixeira <gui.cor.tei@gmail.com>
2021-10-24 13:08:05 -0300

    fix github pages basename

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Routes from './routes';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="gostack_github_explorer">
         <Routes />
       </BrowserRouter>
       <GlobalStyle />
